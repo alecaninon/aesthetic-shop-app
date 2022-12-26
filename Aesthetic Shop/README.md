68279fd4f6b9d97729a8478f2616597505cf8d3e
cipher <alecaninon@gmail.com>
2022-12-27 03:51:44 +0800

    init commit

---
+++ Aesthetic Shop/README.md
@@ -0,0 +1,28 @@
+
+# Aesthetic Shop App
+
+Here is my new Shop App flutter Project, which is Aesthetic Shop Application.
+
+
+## Roadmap
+- Dribble
+
+- Dart
+
+- Flutter
+
+
+## Screenshots
+### Login Page
+
+![App Screenshot](https://github.com/rafipsv/E-Commerce-UI-1/blob/master/assets/ScreenShots/tia1642985103064814289.png?raw=true)
+
+### Home Page
+
+![App Screenshot](https://github.com/rafipsv/E-Commerce-UI-1/blob/master/assets/ScreenShots/tia5025187580502082547.png?raw=true)
+
+### Categories Page
+
+![App Screenshot](https://github.com/rafipsv/E-Commerce-UI-1/blob/master/assets/ScreenShots/tia4234107397884408244.png?raw=true)
+
+
---
+++ Aesthetic Shop/lib/ShopPage/ShopPage.dart
@@ -0,0 +1,60 @@
+// ignore_for_file: file_names
+import 'package:aesthetic_shop_app/Data/Content.dart';
+import 'package:aesthetic_shop_app/ShopPage/components/Categories.dart';
+import 'package:aesthetic_shop_app/ShopPage/components/CategoryTitle.dart';
+import 'package:aesthetic_shop_app/ShopPage/components/ShopWall.dart';
+import 'package:flutter/material.dart';
+
+class ShopPage extends StatefulWidget {
+  const ShopPage({super.key});
+
+  @override
+  State<ShopPage> createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: const [
+          /// this is for showing wall of shop page.
+          ShopWall(),
+
+          /// this is for Showing Category Titles.
+          CategoryTitle(
+            title: "Categories",
+            buttonText: "All",
+          ),
+
+          /// this is for showing category widget.
+          Categoris(
+            height: 170,
+            width: 140,
+            categories: Content.categories,
+            categoriesTitle: Content.categoriesTitle,
+            image: "assets/images/category",
+          ),
+
+          /// this is for litle margin.
+          SizedBox(height: 30),
+
+          /// this is for Showing Category Titles.
+          CategoryTitle(
+            title: "Best Selling by Categories",
+            buttonText: "All",
+          ),
+          Categoris(
+            height: 180,
+            width: 210,
+            categories: Content.bestSell,
+            categoriesTitle: Content.bestSellTitle,
+            image: "assets/images/bestsell",
+          ),
+          SizedBox(height: 50),
+        ],
+      ),
+    );
+  }
+}
